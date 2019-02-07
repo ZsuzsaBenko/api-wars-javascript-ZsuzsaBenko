10c6528d5b8222ccf7432d81d2a144bd094f7c8b
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-02-07 08:11:09 +0100

    Refactoring.

--- static/js/modules/api-wars.js
+++ static/js/modules/api-wars.js
@@ -2,69 +2,61 @@ import {fetchData} from "./ajax.js";
 import {addCommas} from "./helper-functions.js";
 
 
-function displayPlanets(response){
+function displayPlanets(response) {
+
+    let nextButton = document.querySelector("#next");
+    let prevButton = document.querySelector("#prev");
+    setButtonState(nextButton, response.next);
+    setButtonState(prevButton, response.previous);
+
     let planets = response.results;
-    let features = ["name", "diameter", "climate", "terrain", "surface_water", "population"];
+    let features = ["name", "diameter", "climate", "terrain", "surface water", "population", "residents"];
     let table = document.querySelector(".planets");
+
+    let thead = document.createElement("thead");
+    for (let feature of features) {
+        let th = document.createElement("th");
+        feature = feature[0].toUpperCase() + feature.slice(1);
+        th.innerText = feature;
+        thead.appendChild(th);
+    }
+    table.appendChild(thead);
+
     let tbody = document.createElement("tbody");
-    tbody.setAttribute("id", "tbody");
-    for (let i = 0; i < planets.length; i++){
+    for (let i = 0; i < planets.length; i++) {
         let tr = document.createElement("tr");
-        for (let j = 0; j < features.length; j++){
+        for (let j = 0; j < features.length; j++) {
             let td = document.createElement("td");
-            if (planets[i][features[j]] === "unknown"){
+            if (planets[i][features[j]] === "unknown") {
                 td.innerText = "unknown"
-            }
-            else if (features[j] === "diameter"){
+            } else if (features[j] === "diameter") {
                 td.innerText = addCommas(planets[i][features[j]]) + " km";
-            }
-            else if (features[j] === "surface_water") {
-                td.innerText = planets[i][features[j]] + "%";
-            }
-            else if (features[j] === "population"){
+            } else if (features[j] === "surface water") {
+                td.innerText = planets[i]["surface_water"] + "%";
+            } else if (features[j] === "population") {
                 td.innerText = addCommas(planets[i][features[j]]);
-            }
-            else {
+            } else if (features[j] === "residents") {
+                td.innerText = "residents";
+            } else {
                 td.innerText = planets[i][features[j]];
             }
             tr.appendChild(td);
         }
         tbody.appendChild(tr);
     }
-    table.innerHTML = createThead();
     table.appendChild(tbody);
+}
 
 
-    let nextButton = document.querySelector("#next");
-    let prevButton = document.querySelector("#prev");
 
-    if(response.next){
-        nextButton.removeAttribute("disabled");
+function setButtonState(button, condition) {
+    if (condition) {
+        button.removeAttribute("disabled");
     } else {
-        nextButton.setAttribute("disabled", "disabled");
-    }
-
-    if (response.previous){
-        prevButton.removeAttribute("disabled");
-    } else {
-        prevButton.setAttribute("disabled", "disabled");
+        button.setAttribute("disabled", "disabled");
     }
 }
 
-function createThead(){
-    return `
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Diameter</th>
-                <th>Climate</th>
-                <th>Terrain</th>
-                <th>Surface water</th>
-                <th>Population</th>
-            </tr>
-        </thead>
-    `
-}
 
 function showNextOrPrev(button, num){
     let table = document.querySelector(".planets");
