0845b4f2f1a5d43fe79f497e3b11efefff7af7c8
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-02-22 09:02:58 +0100

    Minor correction.

--- server.py
+++ server.py
@@ -75,7 +75,10 @@ def route_voted_planets():
 
 
 def main():
-    app.run(
-	debug=True,
-        host="0.0.0.0",
-        port=8000)
+    app.run(debug=True,
+            host="0.0.0.0",
+            port=8000)
+
+
+main()
+
